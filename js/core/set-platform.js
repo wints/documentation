f2f0b8bc81ce797790b8d694c46cdf64ba129f38
Jake Adams <jakecadams@gmail.com>
2015-04-02 23:16:06 -0700

    add a default per page

--- js/core/set-platform.js
+++ js/core/set-platform.js
@@ -1,3 +1,12 @@
 function setPlatform(platform) {
+	console.log('set platform:', platform);
 	localStorage.setItem('platform', platform);
+}
+
+function getPlatform() {
+	console.log('get platform:', localStorage.getItem('platform'));
+}
+
+function setHeaderToPlatform() {
+	console.log('current platform: ', localStorage.getItem('platform'));
 }
