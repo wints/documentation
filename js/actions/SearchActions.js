0a31e856442fdfc10c1a2aef02a2953a61604cea
MaxLevenson <max@branchmetrics.io>
2015-07-15 15:13:01 -0700

    Switched top5 searching from action to store

--- js/actions/SearchActions.js
+++ js/actions/SearchActions.js
@@ -1,86 +1,6 @@
-var superagent = require('superagent'),
-	lunr = require('lunr'),
-	path = require('path');
+var superagent = require('superagent');
 
-var alt = require('../support/alt'),
-	utils = require('../search/utils');
-
-// Take in the form data and returns whether any of the words are ios/android specific to choose which index to search
-function platformFromQuery(query, data) {
-	var words = query.split(' ');
-	for (var i = 0; i < words.length; i++) {
-		if (data.platform_terms.ios.indexOf(words[i]) > -1) {
-			return 'ios';
-		}
-		else if (data.platform_terms.android.indexOf(words[i]) > -1) {
-			return 'android';
-		}
-	}
-	return 'none';
-}
-
-// Returns whether the current query is platform specific
-function indexSource(term, data) {
-	if (platformFromQuery(term, data) == 'ios') {
-		return [ data.indexes.ios, 'ios' ];
-	}
-	else if (platformFromQuery(term, data) == 'android') {
-		return [ data.indexes.android, 'android' ];
-	}
-	else {
-		return [ data.indexes.default, 'default' ];
-	}
-}
-
-function search(term, data) {
-	var dump = indexSource(term, data);
-	var index = lunr.Index.load(dump[0]);
-	var subsections = data.JSON_data[dump[1]].map(function(raw) {
-		return {
-			id: raw.id,
-			title: raw.title,
-			body: raw.body,
-			url: raw.url
-		};
-	});
-
-	var results = index.search(term).map(function(result) {
-		return subsections.filter(function(q) { return q.id == result.ref; })[0];
-	});
-
-	return results;
-}
-
-// Gives the title of the page that the result is on
-function getResultOrigin(result) {
-	var origin = path.basename(path.dirname(result.url));
-	if (origin == 'ios' || origin == 'android') {
-		origin = path.basename(path.dirname(path.dirname(result.url)));
-	}
-	var parts = origin.replace(/_/g, ' ');
-
-	return parts.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-}
-
-// Gives a context to the first search term within the result
-function getContext(result, accuracy, query) {
-	var term = utils.firstWord(query);
-	var body = result.title.split(' ').concat(result.body.split(' '));
-	var index_term = body.indexOf(term);
-	var pre_context = [],
-		post_context = [];
-	if (index_term == -1) {
-		term = utils.isSubstringArray(body, term);
-		index_term = body.indexOf(term);
-	}
-	for (var i = 0; i < accuracy; i++) {
-		pre_context.push(body[index_term - (accuracy - i)]);
-	}
-	for (var j = 0; j < accuracy; j++) {
-		post_context.push(body[index_term + j + 1]);
-	}
-	return [ pre_context.toString().replace(/,/g, ' ') + ' ', term, ' ' + post_context.toString().replace(/,/g, ' ') ];
-}
+var alt = require('../support/alt');
 
 var SearchActions = function() {
 	return {
@@ -91,18 +11,8 @@ var SearchActions = function() {
 				self.dispatch(JSON.parse(res.text));
 			});
 		},
-
-		// Returns the top 5 results of search
-		top5: function(term, data) {
-			var results = search(term, data);
-
-			var top5 = [];
-			for (var i = 0; i < 5 && i < results.length; i++) {
-				top5.push(results[i]);
-				results[i].origin = getResultOrigin(results[i]);
-				results[i].context = getContext(results[i], 7, term);
-			}
-			this.dispatch([ top5, term ]);
+		top5: function(query) {
+			this.dispatch(query);
 		}
 	};
 };
--- js/stores/SearchStore.js
+++ js/stores/SearchStore.js
@@ -1,6 +1,99 @@
+var lunr = require('lunr'),
+	path = require('path');
+
 var alt = require('../support/alt'),
+	utils = require('../search/utils'),
 	SearchActions = require('../actions/SearchActions');
 
+// Take in the form data and returns whether any of the words are ios/android specific to choose which index to search
+function platformFromQuery(query, data) {
+	var words = query.split(' ');
+	for (var i = 0; i < words.length; i++) {
+		if (data.platform_terms.ios.indexOf(words[i]) > -1) {
+			return 'ios';
+		}
+		else if (data.platform_terms.android.indexOf(words[i]) > -1) {
+			return 'android';
+		}
+	}
+	return 'none';
+}
+
+// Returns whether the current query is platform specific
+function indexSource(term, data) {
+	if (platformFromQuery(term, data) == 'ios') {
+		return [ data.indexes.ios, 'ios' ];
+	}
+	else if (platformFromQuery(term, data) == 'android') {
+		return [ data.indexes.android, 'android' ];
+	}
+	else {
+		return [ data.indexes.default, 'default' ];
+	}
+}
+
+function search(term, data) {
+	var dump = indexSource(term, data);
+	var index = lunr.Index.load(dump[0]);
+	var subsections = data.JSON_data[dump[1]].map(function(raw) {
+		return {
+			id: raw.id,
+			title: raw.title,
+			body: raw.body,
+			url: raw.url
+		};
+	});
+
+	var results = index.search(term).map(function(result) {
+		return subsections.filter(function(q) { return q.id == result.ref; })[0];
+	});
+
+	return results;
+}
+
+// Returns the top 5 results of search
+function top5(term, data) {
+	var results = search(term, data);
+
+	var top5_results = [];
+	for (var i = 0; i < 5 && i < results.length; i++) {
+		results[i].origin = getResultOrigin(results[i]);
+		results[i].context = getContext(results[i], 7, term);
+		top5_results.push(results[i]);
+	}
+	return top5_results;
+}
+
+// Gives the title of the page that the result is on
+function getResultOrigin(result) {
+	var origin = path.basename(path.dirname(result.url));
+	if (origin == 'ios' || origin == 'android') {
+		origin = path.basename(path.dirname(path.dirname(result.url)));
+	}
+	var parts = origin.replace(/_/g, ' ');
+
+	return parts.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+}
+
+// Gives a context to the first search term within the result
+function getContext(result, accuracy, query) {
+	var term = utils.firstWord(query);
+	var body = result.title.split(' ').concat(result.body.split(' '));
+	var index_term = body.indexOf(term);
+	var pre_context = [],
+		post_context = [];
+	if (index_term == -1) {
+		term = utils.isSubstringArray(body, term);
+		index_term = body.indexOf(term);
+	}
+	for (var i = 0; i < accuracy; i++) {
+		pre_context.push(body[index_term - (accuracy - i)]);
+	}
+	for (var j = 0; j < accuracy; j++) {
+		post_context.push(body[index_term + j + 1]);
+	}
+	return [ pre_context.toString().replace(/,/g, ' ') + ' ', term, ' ' + post_context.toString().replace(/,/g, ' ') ];
+}
 var SearchStore = function() {
 	return {
 		displayName: 'SearchStore',
@@ -20,16 +113,20 @@ var SearchStore = function() {
 			this.state.isLoaded = true;
 		},
 
-		onTop5: function(top5_term) {
+		onTop5: function(query) {
 			// Checks if the query is empty
-			if (!top5_term[1].length) {
+			if (!query.length) {
 				this.state.results = [];
 			}
 			// Makes sure results stay showing if the next letter added/removed from the query doesn't turn up any search results
-			else if (top5_term[0].length) {
-				this.state.results = top5_term[0];
+			else {
+				var results = top5(query, this.state.indexes);
+				if (results.length) {
+					this.state.results = results;
+				}
 			}
 		}
+
 	};
 };
 
