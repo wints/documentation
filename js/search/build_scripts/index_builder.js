4ab4d7b1dd68ecaa9f412c954e87f0882aee2d9e
Jake Adams <jakecadams@gmail.com>
2015-07-15 18:17:36 -0700

    fix dir for search_build

--- js/search/build_scripts/index_builder.js
+++ js/search/build_scripts/index_builder.js
@@ -8,7 +8,7 @@ var utils = require('../utils'),
 
 var __dirname;
 
-var directoryPaths = [ path.resolve(__dirname, '../../../_site/recipes'), path.resolve(__dirname, '../../../_site/references') ];
+var directoryPaths = [ path.resolve(__dirname, '../../../recipes'), path.resolve(__dirname, '../../../references') ];
 
 // Creates an object of three different arrays of objects for default, ios, and android
 // directories: the directories to gather JSON data from, defaults to recipes, references, and overviews
